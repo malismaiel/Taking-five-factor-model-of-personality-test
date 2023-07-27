d8f30b51632f96c1ca2990cb2181bb23f6626189
malismaiel <alismaielmurtadha@gmail.com>
2023-07-27 23:03:23 +0500

    repo
    ..

---
+++ api/lib/connect-to-db.js
@@ -0,0 +1,19 @@
+const MongoClient = require('mongodb').MongoClient
+
+let cachedDb = null
+
+const uri = process.env.MONGODB_URI
+
+module.exports = async () => {
+  if (cachedDb) return cachedDb
+
+  const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+  const db = await client.db(new URL(uri).pathname.substr(1))
+
+  cachedDb = db
+
+  console.log('Connected to MongoDB');
+
+  return db
+}
